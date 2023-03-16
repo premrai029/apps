711e57558001f772e7ed89239713b7054366122a
Cyber <spookys459@outlook.com>
2023-03-16 22:47:35 +0545

    finalized bases

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: const Text('DE-TECH')),
-          backgroundColor: const Color.fromARGB(232, 122, 32, 233),
-        ),
-        body: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-                child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('get it'),
-            ))
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Center(child: const Text('DE-TECH')),
+            backgroundColor: const Color.fromARGB(232, 122, 32, 233),
+          ),
+          body: Stack(children: [
+            Image.asset(
+              'assets/images/one.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(100),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('get it'),
+                  ),
+                ))
+              ],
+            ),
+          ])),
     );
   }
 }
