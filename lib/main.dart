cfe635673854fafad013cc33c44228e07805c729
Cyber <spookys459@outlook.com>
2023-03-17 19:15:11 +0545

    not ready

--- lib/main.dart
+++ lib/main.dart
@@ -11,100 +11,96 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          extendBody: true,
-          appBar: AppBar(
-            title: const Center(child: Text('DE-TECH')),
-            backgroundColor: Colors.transparent,
-            elevation: 0, //removes shadow from appbar
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/uio.jpg'),
-                  fit: BoxFit.cover,
-                ),
+        extendBody: true,
+        appBar: AppBar(
+          title: const Center(child: Text('DE-TECH')),
+          backgroundColor: Colors.transparent,
+          elevation: 0, //removes shadow from appbar
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/uio.jpg'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.assignment_outlined),
-                label: 'My Logs',
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.assignment_outlined),
+              label: 'My Logs',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.rocket_launch_outlined),
+              label: 'Events',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.tips_and_updates_outlined),
+              label: 'Others',
+            ),
+          ],
+        ),
+        drawer: const Drawer(child: Text('yell')),
+        body: Stack(children: [
+          Image.asset(
+            'assets/images/get.jpg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                child: SizedBox(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('get it'),
+                  ),
+                ),
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.rocket_launch_outlined),
-                label: 'Events',
+              Expanded(
+                child: SizedBox(
+                  child: ElevatedButton(
+                    child: const Text('get it'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const MyApp(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.tips_and_updates_outlined),
-                label: 'Others',
+              Expanded(
+                child: SizedBox(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('get it'),
+                  ),
+                ),
               ),
             ],
           ),
-          body: Stack(children: [
-            Image.asset(
-              'assets/images/get.jpg',
-              fit: BoxFit.cover,
-              height: double.infinity,
-              width: double.infinity,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('get it'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('get it'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('get it'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('get it'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('get it'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: SizedBox(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('get it'),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ])),
+        ]),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
